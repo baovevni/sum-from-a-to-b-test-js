f2d7acfa15b5ca3f9ad26ef3597f1d16e6f4a6a5
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-03 20:56:54 -0400

    recursive tasks completed

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return fromN;
+  } else {
+    return fromN + sum(fromN + 1, toN)
+  }
 }
 
 module.exports = sum;
+
+// sum(1, 3) => return 1 + sum (1+1, 3) => return 1 + 5
+// sum(2, 3) => return 2 + sum (2+1, 3) => return 2 + 3
+// sum(3, 3) => return 3
